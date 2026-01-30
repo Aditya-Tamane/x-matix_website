cf57c82215cfe9f2df4ee5d5304a2384e7c557e8
Bhubhrit  Sharma <bhubhrit.s@dealermatix.com>
2026-01-30 12:45:16 +0530

    ui fixes

--- src/pages/home1.jsx
+++ src/pages/home1.jsx
@@ -4,6 +4,7 @@ import RequestDemo from "../components/RequestDemo";
 import UseCases from "../components/UseCases";
 import Why from "../components/Why";
 import How from "../components/How";
+import '../styles/home.css';
 
 export default function Home1() {
     return(
