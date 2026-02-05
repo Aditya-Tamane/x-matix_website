bf3496e0ba53f900d37b00994ee4c250d4383963
Bhubhrit  Sharma <bhubhrit.s@dealermatix.com>
2026-02-05 18:34:19 +0530

    cards nav fixed

--- src/components/Domains.jsx
+++ src/components/Domains.jsx
@@ -1,4 +1,5 @@
 import { AdjustmentsVerticalIcon, ArchiveBoxIcon, ArrowLeftEndOnRectangleIcon, BuildingStorefrontIcon, ChartBarIcon, CircleStackIcon, ComputerDesktopIcon, DocumentCurrencyRupeeIcon, MapIcon, ShieldCheckIcon, ShoppingCartIcon, UserGroupIcon, UsersIcon, WrenchIcon } from "@heroicons/react/24/outline";import { Card } from "./common/Card/card"
+import Link from "next/link";
 
 const whatYouCanRun = {
   title: "What you can run",
@@ -13,21 +14,21 @@ const whatYouCanRun = {
     },
   ],
   cardsData : [
-    { title: "Sales", desc: "End-to-end lead-to-cash execution", icon: <ShoppingCartIcon width={24} height={24} /> },
-    { title: "Field Sales", desc: "Distributed sales execution in the field", icon: <MapIcon width={24} height={24} /> },
-    { title: "Service", desc: "End-to-end service execution", icon: <WrenchIcon width={24} height={24} /> },
-    { title: "Field Service", desc: "End-to-end service execution", icon: <AdjustmentsVerticalIcon width={24} height={24} /> },
-    { title: "Warranty", desc: "Automated warranty and claims execution", icon: <ShieldCheckIcon width={24} height={24} /> },
+    { title: "Sales", href:"sales", desc: "End-to-end lead-to-cash execution", icon: <ShoppingCartIcon width={24} height={24} /> },
+    { title: "Field Sales", href:"field-sales", desc: "Distributed sales execution in the field", icon: <MapIcon width={24} height={24} /> },
+    { title: "Incentives", href:"incentives", desc: "End-to-end service execution", icon: <WrenchIcon width={24} height={24} /> },
+    { title: "Field Service", href:"field-services", desc: "End-to-end service execution", icon: <AdjustmentsVerticalIcon width={24} height={24} /> },
+    { title: "Warranty", href:"warrenty", desc: "Automated warranty and claims execution", icon: <ShieldCheckIcon width={24} height={24} /> },
 
-    { title: "Warehouse", desc: "Real-time warehouse execution", icon: <BuildingStorefrontIcon width={24} height={24} /> },
-    { title: "Inventory", desc: "Continuous inventory accuracy", icon: <ArchiveBoxIcon width={24} height={24} /> },
-    { title: "Procurement", desc: "Connected procurement execution", icon: <ArrowLeftEndOnRectangleIcon width={24} height={24} /> },
-    { title: "Finance", desc: "Execution-linked operational finance", icon: <CircleStackIcon width={24} height={24} /> },
-    { title: "Payroll", desc: "Accurate, compliant payroll execution", icon: <DocumentCurrencyRupeeIcon width={24} height={24} /> },
+    { title: "Warehouse", href:"analytics", desc: "Real-time warehouse execution", icon: <BuildingStorefrontIcon width={24} height={24} /> },
+    { title: "Inventory", href:"inventory", desc: "Continuous inventory accuracy", icon: <ArchiveBoxIcon width={24} height={24} /> },
+    { title: "Procurement", href:"procurement", desc: "Connected procurement execution", icon: <ArrowLeftEndOnRectangleIcon width={24} height={24} /> },
+    { title: "Finance", href:"finance", desc: "Execution-linked operational finance", icon: <CircleStackIcon width={24} height={24} /> },
+    { title: "Payroll", href:"payroll", desc: "Accurate, compliant payroll execution", icon: <DocumentCurrencyRupeeIcon width={24} height={24} /> },
 
-    { title: "People", desc: "Workforce execution and management", icon: <UserGroupIcon width={24} height={24} /> },
-    { title: "Partners", desc: "Workforce execution and management", icon: <UsersIcon width={24} height={24} /> },
-    { title: "Analytics", desc: "Execution-driven operational insights", icon: <ChartBarIcon width={24} height={24} /> },
+    { title: "People", href:"people", desc: "Workforce execution and management", icon: <UserGroupIcon width={24} height={24} /> },
+    { title: "Partners", href:"partners", desc: "Workforce execution and management", icon: <UsersIcon width={24} height={24} /> },
+    { title: "Analytics", href:"analytics", desc: "Execution-driven operational insights", icon: <ChartBarIcon width={24} height={24} /> },
   ]
 };
 
@@ -49,12 +50,13 @@ export default function Domains() {
       </div>
       <div className="domains-grid">
         {topDomains.map((domain) => (
-          <Card
-            key={domain.title}
-            icon={domain.icon}
-            title={domain.title}
-            description={domain.desc}
-          />
+          <Link href={`/products/${domain.href}`} key={domain.title} >
+            <Card
+              icon={domain.icon}
+              title={domain.title}
+              description={domain.desc}
+            />
+          </Link>
         ))}
         <div className="domains-last-row">
           {bottomDomains.map((domain) => (
