d9bfafe42205dd47b75054c163c4ce7f7f8343b6
Aditya <aditya.t@dealermatix.com>
2026-02-20 15:06:51 +0530

    image load fix

--- src/components/common/requestDemo/RequestDemo.jsx
+++ src/components/common/requestDemo/RequestDemo.jsx
@@ -8,7 +8,7 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faArrowRightToBracket } from '@fortawesome/free-solid-svg-icons';
 
 const img = {
-  src: '/images/RequestDemo.png',
+  src: '/images/requestDemo.png',
   alt: 'Business handshake',
 }
 
